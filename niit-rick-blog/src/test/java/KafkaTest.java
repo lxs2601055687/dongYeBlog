f08209cb9e7e4a815642f93b65a0139855bfaaba
凉笙 <2601055687@qq.com>
2024-05-22 14:29:12 +0800

    feat:引入kafka配置

---
+++ niit-rick-blog/src/test/java/KafkaTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KafkaTest {
+    @Test
+    public void test() {
+    }
+}
