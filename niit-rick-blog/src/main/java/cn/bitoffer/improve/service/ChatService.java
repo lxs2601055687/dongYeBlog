672de08ea89116fcc267ebfa16db5de6bb8d1774
凉笙 <2601055687@qq.com>
2024-05-28 20:29:27 +0800

    fixed:将\n\n替换防止浏览器错误认为事件结束

--- niit-rick-blog/src/main/java/cn/bitoffer/improve/service/ChatService.java
+++ niit-rick-blog/src/main/java/cn/bitoffer/improve/service/ChatService.java
@@ -9,6 +9,7 @@ import com.alibaba.dashscope.exception.InputRequiredException;
 import com.alibaba.dashscope.exception.NoApiKeyException;
 import com.alibaba.dashscope.utils.JsonUtils;
 import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -16,40 +17,51 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Service
 public class ChatService {
     private static final Logger logger = LoggerFactory.getLogger(ChatService.class);
-    private static final long SSE_TIMEOUT = TimeUnit.MINUTES.toMillis(10); // 10分钟超时
-
-    private static void handleGenerationResult(ApplicationResult result, SseEmitter emitter) throws Exception {
-        String content = String.valueOf(result.getOutput().getText());  // Adjust this based on the actual structure of ApplicationResult
-        logger.info("Received message: {}", JsonUtils.toJson(result));
-        emitter.send(SseEmitter.event().data(content));
-    }
+    private static final long SSE_TIMEOUT = TimeUnit.MINUTES.toMillis(2); // 10分钟超时
 
     public SseEmitter streamCallWithSseEmitter(Message userMsg) throws NoApiKeyException, ApiException, InputRequiredException {
         SseEmitter emitter = new SseEmitter(SSE_TIMEOUT);
         Application app = new Application();
         ApplicationParam param = buildGenerationParam(userMsg);
+        int i = 0;
 
-        CompletableFuture.runAsync(() -> {
-            try {
-                Flowable<ApplicationResult> resultFlowable = app.streamCall(param);
-                resultFlowable.blockingForEach(result -> {
-                    try {
-                        handleGenerationResult(result, emitter);
-                    } catch (Exception e) {
-                        emitter.completeWithError(e);
-                    }
-                });
-                emitter.complete();
-            } catch (Exception e) {
-                emitter.completeWithError(e);
-            }
-        });
+        try {
+            Flowable<ApplicationResult> resultFlowable = app.streamCall(param);
+            Disposable disposable = resultFlowable.subscribe(result -> {
+                try {
+                    handleGenerationResult(result, emitter);
+                } catch (Exception e) {
+                    System.out.println(e);
+                    emitter.completeWithError(e);
+                }
+            });
+            emitter.onCompletion(disposable::dispose); // 取消订阅流
+            emitter.onTimeout(emitter::complete); // 处理超时情况
+        } catch (Exception e) {
+            emitter.completeWithError(e);
+        }
+
+        // 添加超时处理逻辑
+        emitter.onTimeout(() -> emitter.completeWithError(new TimeoutException("Connection timed out")));
 
         return emitter;
+    }
+
+    private static void handleGenerationResult(ApplicationResult result, SseEmitter emitter) {
+        try {
+            String content = String.valueOf(result.getOutput().getText()); // 修改这里获取消息内容的方式
+            content = content.replace("\n\n", "\\sb");
+            content = content.replace("\n", "\\sb");
+            System.out.println("Received message-----------------------------: " + JsonUtils.toJson(content));
+            emitter.send(SseEmitter.event().data(content));
+        } catch (Exception e) {
+            emitter.completeWithError(e);
+        }
     }
 
     private static ApplicationParam buildGenerationParam(Message userMsg) {
